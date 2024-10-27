30a15e15e827bf24be59bbf40718e64e5c4668fa
Dennis <dennisdiepolder@outlook.com>
2024-10-27 13:33:06 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
 import Image from "next/image";
+import { BackgroundBeams } from "./components/ui/background-beams";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaLinkedin } from "react-icons/fa6";
+import { IoIosMail } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    return (
+      <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+        <div className="max-w-2xl mx-auto p-4 flex flex-col justify-center items-center">
+          <h1 className="relative z-10 text-3xl md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 text-center font-sans font-bold">
+            Hi, my name is Dennis Diepolder
+          </h1>
+          <p className="text-neutral-500 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+            I offer customized solutions for websites, platforms, and cloud infrastructures. With experience in serverless web applications, containerization, and automation tools, I ensure your projects are efficient and scalable. Always looking for new challenges and opportunities, I support you in bringing your visions to life.
+          </p>
+          <div className="relative w-[200px] h-[200px] rounded-full overflow-hidden border flex items-center justify-center z-50 mt-4">
+            <Image 
+              src={'/dennisdiepolder.jpg'}
+              layout="fill" 
+              objectFit="cover"
+              alt="Dennis Diepolder"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <p className="text-2xl mt-4 uppercase">Contact me</p>
+          <div className="flex space-x-6 mt-4 text-neutral-300 z-50">
+            <Link href="https://www.linkedin.com/in/dennis-diepolder-2a5a98276/" target="_blank" aria-label="LinkedIn" className="hover:scale-105 transition ease-in-out duration-100">
+              <FaLinkedin size={40} />
+            </Link>
+            <Link href="https://wa.me/+41796108273" target="_blank" aria-label="WhatsApp" className="hover:scale-105 transition ease-in-out duration-100">
+              <IoLogoWhatsapp size={40} />
+            </Link>
+            <Link href="mailto:info@dennisdiepolder.com" target="_blank" aria-label="Email" className="hover:scale-105 transition ease-in-out duration-100">
+              <IoIosMail size={40} />
+            </Link>
+            <Link href='https://github.com/dyprodg' target="_blank" aria-label="GitHub" className="hover:scale-105 transition ease-in-out duration-100">
+              <FaGithub size={40} />
+            </Link>
+          </div>
+          <div className="text-neutral-500 mt-2 text-sm text-center">
+            <p>Phone: +41 796108273</p>
+            <p>Email: <a href="mailto:info@dennisdiepolder.com" className="underline">info@dennisdiepolder.com</a></p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <BackgroundBeams />
+      </div>
+    );
 }
